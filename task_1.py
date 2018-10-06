7cbb911afd1996114b82744fe8785b940fba9562
Volodymyr Shchodryi <shodryj1992@gmail.com>
2018-10-06 16:19:09 +0300

    fist task lesson 1

---
+++ task_1.py
@@ -0,0 +1,8 @@
+
+a = input ("What is your name?")
+b = input ("How old are you?")
+c = input ("Where do you live?")
+
+print ("Hello {0}". format(a))
+print ("Your age is {0}". format (b))
+print ("You live in {0}". format(c))
