07b9d256e34e1d906e14137be5d2d57c28fdb098
Volodymyr Shchodryi <shodryj1992@gmail.com>
2018-10-13 15:50:32 +0300

    codewars_1

---
+++ Number_ to_ a_string.py
@@ -0,0 +1,8 @@
+# def number_to_string(num):
+#     return str(num)
+# print (number_to_string(56))
+
+def reverse(st):
+    return ' '.join(reversed(st.split(' ')))
+st = "hhhhhh sssss qqqqq"
+print(reverse(st))
