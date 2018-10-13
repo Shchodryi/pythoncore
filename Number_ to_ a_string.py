f2071548120acb090c9156325c3d68840989abb0
Shchodryi <shodryj1992@gmail.com>
2018-10-13 15:51:54 +0300

    delete

--- Number_ to_ a_string.py
+++
@@ -1,8 +0,0 @@
-# def number_to_string(num):
-#     return str(num)
-# print (number_to_string(56))
-
-def reverse(st):
-    return ' '.join(reversed(st.split(' ')))
-st = "hhhhhh sssss qqqqq"
-print(reverse(st))
