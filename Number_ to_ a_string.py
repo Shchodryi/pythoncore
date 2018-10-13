3fe051789922a591136d38de473227b08c0dbca1
Volodymyr Shchodryi <shodryj1992@gmail.com>
2018-10-13 15:59:38 +0300

    task 2_1

--- Number_ to_ a_string.py
+++ Number_ to_ a_string.py
@@ -1,8 +1,3 @@
-# def number_to_string(num):
-#     return str(num)
-# print (number_to_string(56))
-
-def reverse(st):
-    return ' '.join(reversed(st.split(' ')))
-st = "hhhhhh sssss qqqqq"
-print(reverse(st))
+def number_to_string(num):
+    return (str(num))
+print (number_to_string(45))
