6210fdc8c2423ace7dee17b317a4b797d4249b48
Shchodryi <shodryj1992@gmail.com>
2018-10-14 16:59:34 +0300

    Add files via upload

---
+++ task1_2.py
@@ -0,0 +1,10 @@
+num1 = int(input("Write first number: "))
+num2 = int(input("Write second number: "))
+plus = num1 + num2
+minus = num1 - num2
+eval = num1 * num2
+diver = num1 / num2
+print ("{0} + {1} = {2}". format(num1,num2,plus))
+print ("{0} - {1} = {2}". format(num1,num2,minus))
+print ("{0} * {1} = {2}". format(num1,num2,eval))
+print ("{0} / {1} = {2}". format(num1,num2,diver))
